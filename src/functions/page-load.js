64a8618080daecf6b4ff54bb6552f474cd26aff7
Andrew Dutson <Dutsandrew@gmail.com>
2022-04-25 12:00:03 -0600

    added preliminary files to create a website using weback and javascript modules

---
+++ src/functions/page-load.js
@@ -0,0 +1,5 @@
+const pageLoadModule = (function() {
+    const content = document.querySelector('#content');
+
+    
+})();
---
+++ src/index.js
@@ -0,0 +1,3 @@
+console.log("Check, Check, Check");
+
+import './style.css';
