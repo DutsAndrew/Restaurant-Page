4080b3f95a7df561a89374de58738034fecc367e
Andrew Dutson <Dutsandrew@gmail.com>
2022-04-26 16:47:45 -0600

    fixed link between page-load.js and index.js and stylesheet import

--- src/functions/page-load.js
+++ src/functions/page-load.js
@@ -1,5 +1,14 @@
-const pageLoadModule = (function() {
-    const content = document.querySelector('#content');
+const pageLoadModule = () => {
+    const header = document.createElement('div');
+    header.classList.add('header');
 
-    
-})();
+    const paragraphHeader = document.createElement('p');
+    paragraphHeader.textContent = "Espresso Bar";
+    paragraphHeader.classList.add('header-text');
+
+    header.appendChild(paragraphHeader);
+};
+
+export {
+     pageLoadModule
+};
--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
+import './stylesheets/page-load.css';
+import {pageLoadModule} from './functions/page-load.js';
+
 console.log("Check, Check, Check");
 
-import './style.css';
+const buildHomePage = () => {
+    pageLoadModule();
+
+    const content = document.querySelector('#content');
+    content.classList.add('content');
+
+    content.appendChild(header);
+}
+
+buildHomePage();
+
