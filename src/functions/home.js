f5094ab1269b96f19e2795b6e11ea3b1367dad08
Andrew Dutson <Dutsandrew@gmail.com>
2022-05-06 12:47:55 -0600

    event listeners for buttons can clear DOM, cannot add back items

--- src/functions/home.js
+++ src/functions/home.js
@@ -6,17 +6,21 @@ const loadHomePage = () => {
     
     // content items and Website Title
     const content = document.querySelector('#content');
-    content.classList.add('content');
+        content.classList.add('content');
+
+    const homePageDiv = document.createElement('div');
+        homePageDiv.classList.add('appended-pages');
+        homePageDiv.setAttribute('id', 'home-page-div');
 
     const websiteTitle = document.createElement('p');
-    websiteTitle.textContent = "Skrrt Café";
-    websiteTitle.classList.add('content-text');
-    websiteTitle.setAttribute('id', 'website-title');
+        websiteTitle.textContent = "Skrrt Café";
+        websiteTitle.classList.add('content-text');
+        websiteTitle.setAttribute('id', 'website-title');
 
     // Buttons for the menu bar at the top
     const buttonContainer = document.createElement('div');
-    buttonContainer.classList.add('menu-button-container');
-    buttonContainer.setAttribute('id', 'menu-buttons');
+        buttonContainer.classList.add('menu-button-container');
+        buttonContainer.setAttribute('id', 'menu-buttons');
 
         const homeButton = document.createElement('button');
             const homeImage = document.createElement('div');
@@ -56,56 +60,56 @@ const loadHomePage = () => {
 
     // Banner Image for Main Page
     const bannerImage = document.createElement('div');
-    bannerImage.classList.add('banner-img');
-    bannerImage.setAttribute('id', 'banner-img');
+        bannerImage.classList.add('banner-img');
+        bannerImage.setAttribute('id', 'banner-img');
 
     // Store Hours displayed on Home Page
     const storeHoursContainer = document.createElement('div');
         storeHoursContainer.classList.add('store-hours-container');
         storeHoursContainer.setAttribute('id', 'store-hours-container');
-        const storeHoursTable = document.createElement('table');
-        const storeHoursCaption = document.createElement('caption');
-            storeHoursCaption.textContent = "Skkrt Café - Store Hours";
-        const storeHoursTableHead = document.createElement('thead');
-        const storeHoursTableRow = document.createElement('trow');
-        const storeHoursDaycontent = document.createElement('th');
-            storeHoursDaycontent.textContent = "Days Open";
-        const storeHoursHourscontent = document.createElement('th');
-            storeHoursHourscontent.textContent = "Hours";
+            const storeHoursTable = document.createElement('table');
+            const storeHoursCaption = document.createElement('caption');
+                storeHoursCaption.textContent = "Skkrt Café - Store Hours";
+            const storeHoursTableHead = document.createElement('thead');
+            const storeHoursTableRow = document.createElement('trow');
+            const storeHoursDayContent = document.createElement('th');
+                storeHoursDayContent.textContent = "Days Open";
+            const storeHoursHoursContent = document.createElement('th');
+                storeHoursHoursContent.textContent = "Hours";
 
-        const storeHoursTableBody = document.createElement('tbody');
-        const storeHoursTableBodyRowDays = document.createElement('tr');
-        const storeHoursTableBodyRowHours = document.createElement('tr');
+            const storeHoursTableBody = document.createElement('tbody');
+            const storeHoursTableBodyRowDays = document.createElement('tr');
+            const storeHoursTableBodyRowHours = document.createElement('tr');
 
-        const storeDayMonday = document.createElement('td');
-            storeDayMonday.textContent = "Monday";
-        const storeDayTuesday = document.createElement('td');
-            storeDayTuesday.textContent = "Tuesday";
-        const storeDayWednesday = document.createElement('td');
-            storeDayWednesday.textContent = "Wednesday";
-        const storeDayThursday = document.createElement('td');
-            storeDayThursday.textContent = "Thursday";
-        const storeDayFriday = document.createElement('td');
-            storeDayFriday.textContent = "Friday";
-        const storeDaySaturday = document.createElement('td');
-            storeDaySaturday.textContent = "Saturday";
-        const storeDaySunday = document.createElement('td');
-            storeDaySunday.textContent = "Sunday";
+            const storeDayMonday = document.createElement('td');
+                storeDayMonday.textContent = "Monday";
+            const storeDayTuesday = document.createElement('td');
+                storeDayTuesday.textContent = "Tuesday";
+            const storeDayWednesday = document.createElement('td');
+                storeDayWednesday.textContent = "Wednesday";
+            const storeDayThursday = document.createElement('td');
+                storeDayThursday.textContent = "Thursday";
+            const storeDayFriday = document.createElement('td');
+                storeDayFriday.textContent = "Friday";
+            const storeDaySaturday = document.createElement('td');
+                storeDaySaturday.textContent = "Saturday";
+            const storeDaySunday = document.createElement('td');
+                storeDaySunday.textContent = "Sunday";
 
-        const mondayHours = document.createElement('td');
-            mondayHours.textContent = "5:00 AM - 6:00 PM";
-        const tuesdayHours = document.createElement('td');
-            tuesdayHours.textContent = "5:00 AM - 6:00 PM";
-        const wednesdayHours = document.createElement('td');
-            wednesdayHours.textContent = "5:00 AM - 6:00 PM";
-        const thursdayHours = document.createElement('td');
-            thursdayHours.textContent = "5:00 AM - 6:00 PM";
-        const fridayHours = document.createElement('td');
-            fridayHours.textContent = "5:00 AM - 9:00 PM";
-        const saturdayHours = document.createElement('td');
-            saturdayHours.textContent = "5:00 AM - 6:00 PM";
-        const sundayHours = document.createElement('td');
-            sundayHours.textContent = "5:00 AM - 2:00 PM";
+            const mondayHours = document.createElement('td');
+                mondayHours.textContent = "5:00 AM - 6:00 PM";
+            const tuesdayHours = document.createElement('td');
+                tuesdayHours.textContent = "5:00 AM - 6:00 PM";
+            const wednesdayHours = document.createElement('td');
+                wednesdayHours.textContent = "5:00 AM - 6:00 PM";
+            const thursdayHours = document.createElement('td');
+                thursdayHours.textContent = "5:00 AM - 6:00 PM";
+            const fridayHours = document.createElement('td');
+                fridayHours.textContent = "5:00 AM - 9:00 PM";
+            const saturdayHours = document.createElement('td');
+                saturdayHours.textContent = "5:00 AM - 6:00 PM";
+            const sundayHours = document.createElement('td');
+                sundayHours.textContent = "5:00 AM - 2:00 PM";
 
         storeHoursTableBodyRowDays.appendChild(storeDayMonday);
         storeHoursTableBodyRowDays.appendChild(storeDayTuesday);
--- src/index.js
+++ src/index.js
@@ -3,23 +3,64 @@ import './stylesheets/home.css';
 import './stylesheets/menu.css';
 import './stylesheets/about.css';
 import { loadHomePage } from './functions/home.js';
+import { loadMenuPage } from './functions/menu.js';
+import { loadAboutPage } from './functions/about.js';
 
 // Default Load
 loadHomePage();
 
-// Provides access to DOM elements, so they can be removed or kept when menu buttons are clicked
-const content = document.querySelector('#content');
-const menuButtons = document.querySelector('#menu-buttons');
-const websiteTitle = document.querySelector('#website-title');
-const bannerImage = document.querySelector('#banner-img');
-const storeHoursContainer = document.querySelector('#store-hours-container');
+// JS to transition between Home, Menu, and About Pages
+    let currentPage = "home";
+    // DOM elements that remain on the page
+    const contentCheck = document.getElementById('content');
 
-// Menu Button Handlers
-const homeHandler = document.getElementById('home-button');
-    homeHandler.addEventListener('click', switchToHome);
+    // Home Button Handler
+    const homeHandler = document.getElementById('home-button');
+        homeHandler.addEventListener('click', switchToHome);
+        function switchToHome() {
+            if (currentPage == "home") {
+                console.log("Home Page was clicked!");
+                return
+            } else {
+                currentPage = "home";
+                while (contentCheck.firstChild) {
+                    contentCheck.removeChild(contentCheck.firstChild);
+                };
+                loadHomePage();
+                console.log("Home Page was clicked!");
+            }
+        };
 
-    function switchToHome() {
+    // Menu Button Handler
+    const menuHandler = document.getElementById('menu-button');
+        menuHandler.addEventListener('click', switchToMenu);
+        function switchToMenu() {
+            if (currentPage == "menu") {
+                console.log("Menu Page was clicked!");
+                return
+            } else {
+                currentPage = "menu";
+                while (contentCheck.firstChild) {
+                    contentCheck.removeChild(contentCheck.firstChild);
+                };
+                loadMenuPage();
+                console.log("Menu Page was clicked!");
+            }
+        };
 
-        loadHomePage();
-        console.log("Home Button was clicked!");
-    }
+    // About Button Handler
+    const aboutHandler = document.getElementById('about-button');
+        aboutHandler.addEventListener('click', switchToAbout);
+        function switchToAbout() {
+            if (currentPage == "about") {
+                console.log("About Page was clicked!");
+                return
+            } else {
+                currentPage = "about";
+                while (contentCheck.firstChild) {
+                    contentCheck.removeChild(contentCheck.firstChild);
+                };
+                loadAboutPage();
+                console.log("About Page was clicked!");
+            }
+        };
